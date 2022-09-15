408b94ad735aa362723cb0949f44bf0a62bc8eac
bswiercz <swierkdeck@gmail.com>
2022-09-15 21:16:54 +0200

    added Spring support

--- src/main/java/io/bs/gitlist/GitList.java
+++ src/main/java/io/bs/gitlist/GitList.java
@@ -1,5 +1,8 @@
 package io.bs.gitlist;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class GitList {
     public static void main(String[] args) {
     }
